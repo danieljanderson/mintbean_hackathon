6b5cb01dc2def864fc4fe6ecfbf99974fb413342
chiboycalix <igwechinonso77@gmail.com>
2021-07-29 15:35:07 +0100

    setup folder structure

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+import { Button } from '../../components'
+
+const Home = () => {
+  return (<div>
+
+    <Button />
+  </div>)
+}
+
+export default Home
