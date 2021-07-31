da0b2c90a90b57143e568c199f23dd7bdf388cb8
danieljanderson <danieljanderson0616@gmail.com>
2021-07-31 16:55:53 -0400

    single player betting finnished

--- src/blackjackGame.js
+++ src/blackjackGame.js
@@ -6,8 +6,11 @@ class Blackjack{
     
     constructor()
     {
+    // this stores the players data
     this._players = []
+    //this stores dealer data
     this._dealer = new Dealer()
+    // this stores the betting data
     this._pot =[]
     }
     //! getters
@@ -33,20 +36,54 @@ class Blackjack{
     
 
     //it gets a array of player names and then makes new player objects of those players
-    createPlayers(numPlayers){
-        let listOfPlayers = []
-        for (let i = 0; i<=numPlayers.length-1;i++){
-            let player = new Player()
+    // createPlayers(numPlayers){
+    //     let listOfPlayers = []
+    //     for (let i = 0; i<=numPlayers.length-1;i++){
+    //         let player = new Player()
            
-            /*the array name and stringname are making it into a string since the data type that
-            is recieved is an array.
-            */
-            let arrayName = numPlayers[i]
-            let stringName = arrayName.toString()
-            player.name = stringName
-            listOfPlayers.push(player)
+    //         /*the array name and stringname are making it into a string since the data type that
+    //         is recieved is an array.
+    //         */
+    //         let arrayName = numPlayers[i]
+    //         let stringName = arrayName.toString()
+    //         player.name = stringName
+    //         listOfPlayers.push(player)
+    //     }
+    //     this._players = listOfPlayers
+    // }
+    // gets a person object and an amount
+    placeBet(player,amount){
+        let  bet = {
+            PlayerName : player.name,
+            bet : amount 
         }
-        this._players = listOfPlayers
+        let tempPot = this._pot
+        let money = player.money
+            money = money -amount 
+            tempPot.push(bet)
+            this._pot = tempPot
+            
+            player.money = money
+            
+        // this is to find what index the player is in the players array
+       // const index = this._players.name.indexOf(player)
+        // this is to then access the player object from the players
+        //const foundPlayer = this._players[index]
+        //const bettingPlayer = foundPlayer
+        //const bettingAmount = amount
+        // total pot is a copy of this._pot so I am not mutating the actual data
+       // let totalPot = this._pot
+        //this is to subtract the amount from the persons total money.  in other words
+        // if player has a hundred dollars and bets 50 he should have 50 left
+        //bettingPlayer.money = bettingPlayer.money-bettingAmount 
+
+        //the bet should be added to the pot with the persons name on it
+        //const theBetRecord = {
+         //   name: bettingPlayer.name,
+           // amount: bettingAmount
+       // }
+        //totalPot = totalPot.push(theBetRecord)
+        //this._pot = totalPot
     }
 
 }
--- test/blackjack_test.js
+++ test/blackjack_test.js
@@ -6,18 +6,28 @@ const chai = require('chai')
 const expect = chai.expect
 
 describe("testing blackjack",()=>{
-    it("will create a one person blackjack game",()=>{
-        let names = ["Daniel"]
-        let game = new Blackjack()
-        game.createPlayers(names)
-        expect(game.players[0].name).to.deep.equal("Daniel")
+    // it("will create a one person blackjack game",()=>{
+    //     let names = ["Daniel"]
+    //     let game = new Blackjack()
+    //     game.createPlayers(names)
+    //     expect(game.players[0].name).to.deep.equal("Daniel")
 
-    })
-    it("will create a multiplayer, blackjack game",()=>{
-        let names = ["Daniel","Sarah","Tara","Tami"]
+    // })
+    // it("will create a multiplayer, blackjack game",()=>{
+    //     let names = ["Daniel","Sarah","Tara","Tami"]
+    //     let game = new Blackjack()
+    //     game.createPlayers(names)
+    //     expect(game.players.length).to.deep.equal(4)
+    //     //expect(game.players[2].name).to.deep.equal("Tara")
+    // })
+    it("it will place one persons bet",()=>{
+        let Daniel = new Player("Daniel")
+        Daniel.money= 100
         let game = new Blackjack()
-        game.createPlayers(names)
-        expect(game.players.length).to.deep.equal(4)
-        //expect(game.players[2].name).to.deep.equal("Tara")
+        game.placeBet(Daniel,50)
+        expect(Daniel.money).to.deep.equal(50)
+        expect(game.pot[0].PlayerName).to.deep.equal("Daniel")
+        expect(game.pot[0].bet).to.deep.equal(50)
+     
     })
 })
