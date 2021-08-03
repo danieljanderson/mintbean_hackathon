ed5bfd3d0ad6aabcba34a9959e68b2f261cc18c0
danieljanderson <danieljanderson0616@gmail.com>
2021-08-03 17:25:10 -0400

    fixed ace issue

--- src/utils/gameState.js
+++ src/utils/gameState.js
@@ -1,5 +1,7 @@
 export const calcScore = cards => {
   let score = 0;
+  let numberOfAces = 0;
+
   const pointsArr1 = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
   const pointsArr2 = ['jack', 'queen', 'king'];
 
@@ -9,20 +11,38 @@ export const calcScore = cards => {
         score = score + parseInt(card.rank);
       } else if (pointsArr2.includes(card.rank)) {
         score = score + 10;
-      } else if (card.rank === '1') {
-        if (score + 11 <= 21) {
-          score = score + 11;
-        } else {
-          score = score + 1;
-        }
-      }
+       } 
+      else if (card.rank === '1') {
+        numberOfAces = numberOfAces + 1
+        score = score + 11
+      //   if (score + 11 <= 21) {
+      //     score = score + 11;
+      //   } else {
+      //     score = score + 1;
+      //   }
+       }
+       
+
     }
   });
+  // Account for the aces
+  for (let i = 1; i <= numberOfAces; i++) {
+    console.log("number of aces" + numberOfAces)
+    const isBust = score > 21;
+
+    if (isBust) {
+      // try to save
+      score -= 10;
+    }
+
+    numberOfAces -= 1;
+  }
   return score;
 };
 
 export const calcFullScore = cards => {
   let score = 0;
+  let numberOfAces = 0
   const pointsArr1 = ['2', '3', '4', '5', '6', '7', '8', '9', '10'];
   const pointsArr2 = ['jack', 'queen', 'king'];
 
@@ -33,14 +53,23 @@ export const calcFullScore = cards => {
       } else if (pointsArr2.includes(card.rank)) {
         score = score + 10;
       } else if (card.rank === '1') {
-        if (score + 11 <= 21) {
-          score = score + 11;
-        } else {
-          score = score + 1;
-        }
+        numberOfAces = numberOfAces + 1
+        score = score + 11
       }
     });
   }
+  // Account for the aces
+  for (let i = 1; i <= numberOfAces; i++) {
+    const isBust = score > 21;
+
+    if (isBust) {
+      // try to save
+      score -= 10;
+    }
+
+    numberOfAces -= 1;
+  }
+
   return score;
 };
 
